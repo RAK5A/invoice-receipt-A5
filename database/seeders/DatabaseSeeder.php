eb8e988893c967391b07350dad19dbfc79ac7283
RAK5A <raksachea04@gmail.com>
2025-12-15 22:36:52 +0700

    seeddata incase you don't want to create it

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\User;
+use Hash;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -15,11 +16,12 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        User::create([
+            'name' => 'Admin User',
+            'username' => 'admin',
+            'email' => 'admin@step.org',
+            'phone' => '012345678',
+            'password' => Hash::make('admin123'),
         ]);
     }
 }
