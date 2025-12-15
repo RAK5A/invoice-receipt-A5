13092dc2ed92e87aa34e4808c7ae080e04d65019
RAK5A <raksachea04@gmail.com>
2025-12-15 22:49:13 +0700

    Update DatabaseSeeder to create an Admin User

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\User;
+use Hash;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -17,9 +18,12 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        User::create([
+            'name' => 'Admin User',
+            'username' => 'admin',
+            'email' => 'admin@step.org',
+            'phone' => '1234567890',
+            'password' => Hash::make('admin123'),
         ]);
     }
 }
