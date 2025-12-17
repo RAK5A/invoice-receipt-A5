5c0f88320644eed5de1aa0c9496e5ec8787d0a18
RAK5A <raksachea04@gmail.com>
2025-12-17 15:12:17 +0700

    change the style of navbar and cleaning up unused code

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,7 @@
 <?php
 
-use App\Http\Controllers\AuthController;
 use App\Http\Controllers\CustomerController;
 use App\Http\Controllers\DashboardController;
-use App\Http\Controllers\HomeController;
 use App\Http\Controllers\InvoiceController;
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\UserController;
